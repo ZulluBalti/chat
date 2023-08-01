9b74595b7c6390482f3e63918162e91754e67b4e
ZulluBalti <ZulluBalti@gmail.com>
2023-08-01 16:15:34 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/Chat.js'),
+      entry: path.resolve(__dirname, './main.js'),
       name: "@zullu_balti/chat",
       fileName: format => `chat.${format}.js`
     },
